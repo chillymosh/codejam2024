a311e7fc4e771bd9751d640e06f6d2c4dff267bc
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-04-30 19:41:38 -0700

    added 2 tables to schema: teams, team_users

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -10,6 +10,26 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_service_user ON users (service_name, service_user_id);
+    
+CREATE TABLE IF NOT EXISTS teams (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    event_id UUID references events(id),
+    owner_user_id UUID references users(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    name TEXT NOT NULL,
+    visibility TEXT,
+    timezone TEXT,
+    technologies TEXT,
+    availability TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_on TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE('utc'))
+);
+
+CREATE TABLE IF NOT EXISTS team_users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    created_on TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE('utc'))
+    team_id UUID references teams(id) ON DELETE CASCADE ON UPDATE NO ACTION
+    user_id UUID reference users(id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
 
 CREATE TABLE IF NOT EXISTS events (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
